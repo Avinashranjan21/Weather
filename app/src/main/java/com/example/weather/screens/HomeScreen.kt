904299e99479d2c80f39d8d1a6b0e62997adc47d
Avinash Pandey <AvinashRanjan21@Gmail.com>
2023-08-16 12:31:31 +0530

    #Gradle : Grade version updated at libs.versions.toml

---
+++ app/src/main/java/com/example/weather/screens/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.weather.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun HomeScreen() {
+
+}
